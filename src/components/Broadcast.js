71c102ae2dadf3969ba838176fff02e40dad35c6
Steve LeValley <slevalley@gmail.com>
2022-04-20 07:10:06 -0700

    Displaying broadcasts with images

--- src/components/Broadcast.js
+++ src/components/Broadcast.js
@@ -1,14 +1,45 @@
 import React from 'react'
-import withStyles from '@material-ui/styles/withStyles'
+import { Link } from 'react-router-dom';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-const styles = {
+const useStyles = makeStyles((theme) => ({
   card: {
-    display: 'flex'
+    display: "flex",
+    marginBottom: 10
+  },
+  image: {
+    minWidth: 200
+  },
+  content: {
+    padding: 25,
+    objectFit: 'cover'
   }
+}));
+
+function Broadcast({ broadcast }) {
+  console.log(broadcast)
+  const classes = useStyles();
+  const { userImage, body, createdAt, userName, broadcastId, likeCount, commentCount } = broadcast
+
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.image}
+        image={userImage}
+        title='Profile Image' />
+      <CardContent className={classes.content}>
+        <Typography component={Link} to={`/users/${userName}`} variant='h5' color='primary'>{userName}</Typography>
+        <Typography variant='body2' color='textSecondary'>{createdAt}</Typography>
+        <Typography variant='body1'>{body}</Typography>
+      </CardContent>
+
+    </Card>
+  )
 }
 
-function Broadcast ({ broadcast }) {
-  return <div>{broadcast.body}</div>
-}
-
-export default withStyles(styles)(Broadcast)
+export default Broadcast
